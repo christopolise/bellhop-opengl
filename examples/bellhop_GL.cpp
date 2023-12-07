d5a21430268d80b60ea4e4401f76b9d310d4c76f
apal6981 <ashton.palacios1@gmail.com>
2023-12-07 16:45:18 -0700

    a merge destined for something

--- examples/bellhop_GL.cpp
+++ examples/bellhop_GL.cpp
@@ -1,10 +1,8 @@
-#include <glad/glad.h>
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h>
 
-
-
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <ft2build.h>
 
